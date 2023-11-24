46b41e56275781c7bd8e8d1051b3922d9ca49780
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-24 21:04:54 +0530

    * Get request

--- expressjs/index.js
+++ expressjs/index.js
@@ -1,6 +1,9 @@
 const express = require("express") ;
+const bodyParser = require("body-parser") ;
 const app = express() ;
 const port = 4000 ;
+const path = require("path") ;
+app.use(bodyParser.urlencoded({extended: false})) ;
 
 //  Creating server with express
 
@@ -9,10 +12,21 @@ app.listen(port,()=>{
 }) ;
 
 
-//  Get page from request
+//  Get page from request via GET METHOD
 app.get("/about",(req,res)=>{
     res.send("<h1>About Page</h1>") ;
 }) ;
+
+//  Send page to the client  via send function
+app.get("/form",(req,res)=>{
+    res.sendFile(path.join(__dirname+"/index.html")) ;    
+})
+
+//  Post data recieved from form page
+app.post("/form",(req,res)=>{
+    console.log(req.body.email) ; 
+    res.send("<h1>Done</h1>")   
+})
 
 app.get("/contact",(req,res)=>{
     res.send("<h1>Contact Page</h1>") ;

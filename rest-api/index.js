1495b6561882bb11543f9a4ab91c8887fc51d94d
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-27 22:50:53 +0530

    * Create new product API & Enable body parser.

--- rest-api/index.js
+++ rest-api/index.js
@@ -4,16 +4,31 @@ const port = 4000 ;
 const path = require("path") ;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser") ;
+const successStatus = 200 ;
+
+
+//  Enable Body parser to pass response as json
+app.use(bodyParser.urlencoded({extended:false})) 
+app.use(express.json())
 
 //  Create port on definded server
 app.listen(port,()=>{
     console.log("Server started at http:localhost:"+port)
 })
 
+//  Create product schema and model, mongo db 
 const produtSchema = new mongoose.Schema({
     name:String,
     description : String,
     price : Number
 })
-
 const product = new mongoose.model("Product",produtSchema)
+
+//  API for creating new product
+ app.post("/api/v1/product/new",async (req,res)=>{
+    const product = await Product.create(req.body) ;
+    res.status(successStatus).json({
+        success:true,
+        product
+    })
+ })

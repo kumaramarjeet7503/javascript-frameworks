b3c9ec94f177cfd1a53ab6bcee8cb9ac17809c5b
Amarjeet <kumaramarjeet001@gmail.com>
2023-11-27 22:36:22 +0530

    * Creating new conn with mongodb

---
+++ mongodb/index.js
@@ -0,0 +1,29 @@
+const express = require("express") ;
+const app = express() ;
+const port = 4000 ;
+const path = require("path") ;
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/demo").then(()=>{
+    console.log("Connected to mongodb") ;
+}).catch((err)=>{
+    console.log(err);
+})
+
+// const student = new mongoose.Schema({
+//     name:{type:String},
+//     section:String,
+//     course:Number,
+//     active : Boolean
+// })
+
+// const client = new  mongoose.model("Student",student) ;
+
+// const ss = new client({
+//     name:"hem",
+//     section:"10",
+//     course:2,
+//     active:true
+// })
+
+// await ss.save() ;

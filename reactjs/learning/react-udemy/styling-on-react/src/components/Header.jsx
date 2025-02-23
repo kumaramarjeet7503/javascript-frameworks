b1cda97207cc385534148ae81bf2e7d774530034
Amarjeet <kumaramarjeet001@gmail.com>
2025-02-23 18:41:33 +0530

    styled components

--- reactjs/learning/react-udemy/styling-on-react/src/components/Header.jsx
+++ reactjs/learning/react-udemy/styling-on-react/src/components/Header.jsx
@@ -1,6 +1,11 @@
 import logo from '../assets/logo.png';
 import {styled} from 'styled-components'
-styled.p`color: red`
+
+
+
+const RedPara = styled.p`
+color: ${({invalid}) => invalid ? 'red': 'black'}
+`
 
 export default function Header() {
   return (
@@ -9,7 +14,7 @@ export default function Header() {
       <h1 style={{
         color:'black',fontStyle:'italic'
       }} >ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <RedPara  invalid={true}>A community of artists and art-lovers.</RedPara>
     </header>
   );
 }

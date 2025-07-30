b05beeab3fe6507cfa5fdc9f6bd6f1eea7244eaa
Amarjeet <kumaramarjeet001@gmail.com>
2025-07-30 22:26:52 +0530

    feat: using useEffect with dependency

--- reactjs/learning/react-udemy/use-effect-project/src/App.jsx
+++ reactjs/learning/react-udemy/use-effect-project/src/App.jsx
@@ -1,25 +1,38 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
-import {sortPlacesByDistance} from './loc.js'
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+  /** Here is the use case that initially we have to load the data
+   * so we need to execute it only once that is why we have put outside the App component */ 
+  const selectedPlaceList = ()=>{
+    const selectedPlaces = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+    const places = []
+    for(const  id of selectedPlaces){
+       const place = AVAILABLE_PLACES.find((place) => place.id === id);
+       if(place) places.push(place)
+    }
+    return places
+  }
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [availablePlaces, setAvailablePlace] = useState([])
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(selectedPlaceList);
+   const [availablePlaces, setAvailablePlace] = useState([]);
+   const [isModelOpen, setIsModelOpen] = useState(false);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+       setIsModelOpen(true)
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+      setIsModelOpen(false)
   }
 
   function handleSelectPlace(id) {
@@ -32,31 +45,49 @@ function App() {
       /** Here we will going to use localstorage feature of browser to store data in the form of string only
        * The data only be stored in the form of string in it.
        */
-      const selectedPlaces = JSON.parse(localStorage.getItem('selectedPlaces')) || []
-      if(!selectedPlaces.includes(id)) localStorage.setItem('selectedPlaces',JSON.stringify([id,...selectedPlaces]))
+      const selectedPlaces = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+      if (!selectedPlaces.includes(id))
+        localStorage.setItem(
+          "selectedPlaces",
+          JSON.stringify([id, ...selectedPlaces])
+        );
       return [place, ...prevPickedPlaces];
     });
   }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
-      prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
+      prevPickedPlaces.filter((place) => {
+        if(place.id !== selectedPlace.current) return true
+        else{
+          /** Here removing elements from the local storage also works the same ways */
+          const selectedPlaces =  JSON.parse(localStorage.getItem('selectedPlaces')) || []
+          const updatedPlaces = selectedPlaces.filter(placeId => placeId !== selectedPlace.current)
+          localStorage.setItem('selectedPlaces',JSON.stringify(updatedPlaces))
+          return false
+        }
+      })
     );
-    modal.current.close();
+    setIsModelOpen(false)
   }
 
   /** Here this hook will executes when the components loads fully and then it
    *  set/update the state so that the code not get into the infinite loop */
-  useEffect(()=>{
-  navigator.geolocation.getCurrentPosition((position)=>{
-       setAvailablePlace(sortPlacesByDistance(AVAILABLE_PLACES,position.coords.latitude,position.coords.longitude)) 
-  })    
-  },[])
-
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setAvailablePlace(
+        sortPlacesByDistance(
+          AVAILABLE_PLACES,
+          position.coords.latitude,
+          position.coords.longitude
+        )
+      );
+    });
+  }, []);
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={isModelOpen} ref={modal}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -74,7 +105,7 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
